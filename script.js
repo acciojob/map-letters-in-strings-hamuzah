c80d58b24db358001dcf70d571a6adcace4c4b8f
hamuzah <37175583+hamuzah@users.noreply.github.com>
2024-05-08 12:26:52 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function map(str) {
+	let obj = {};
+
+	for(let i=0; i<str.length; i++){
+        let t = str[i];
+		if(obj[t] == undefined){
+			obj[t] = [i]
+		}else{
+			obj[t].push(i);
+		}
+	}
+}
